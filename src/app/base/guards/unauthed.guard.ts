43c377ea1a4e0dacd0061c7522b509729026c60e
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-04-13 22:37:34 +0900

    finish

--- src/app/base/guards/unauthed.guard.ts
+++ src/app/base/guards/unauthed.guard.ts
@@ -2,7 +2,6 @@ import { AuthenticationService } from '../services/authentication.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
