31354c62c03936694e38f8170a297852dc6e8c61
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-04-13 17:41:11 +0900

    add sandbox env and refactor auth module

--- src/app/auth/confirm/confirm.component.spec.ts
+++ src/app/auth/confirm/confirm.component.spec.ts
@@ -8,9 +8,9 @@ describe('ConfirmComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmComponent ]
+      declarations: [ConfirmComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/auth/confirm/confirm.component.ts
+++ src/app/auth/confirm/confirm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../common/authentication.service';
 
 @Component({
   selector: 'app-confirm',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService, private route: ActivatedRoute) { }
 
+  code: string = "";
+  get username() {
+    return this.route.snapshot.queryParamMap.get("username");
+  };
   ngOnInit(): void {
   }
 
+  submit() {
+    if (this.username != null) {
+      this.authenticationService.confirmSignUp({ username: this.username, code: this.code })
+    }
+  }
 }
