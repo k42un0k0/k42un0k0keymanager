3a20260aa02e9bc5922fd19296e66b2baefa1aaa
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-04-20 23:57:44 +0900

    create account card

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,34 +1,21 @@
-import { AccountViewerComponent } from './components/account-viewer/account-viewer.component';
-import { AccountEditorComponent } from './components/account-editor/account-editor.component';
-import { AccountCardComponent } from './components/account-card/account-card.component';
 import { OuterAccountListComponent } from './outer-account-list/outer-account-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { TabComponent } from './components/tab/tab.component';
-import { TabbarComponent } from './components/tabbar/tabbar.component';
-import { AccountModalComponent } from './components/account-modal/account-modal.component';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterModule } from '@angular/router';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
     MainComponent,
-    AccountCardComponent,
-    AccountEditorComponent,
-    AccountViewerComponent,
-    AccountModalComponent,
-    SidebarComponent,
-    TabComponent,
-    TabbarComponent,
     OuterAccountListComponent,
   ],
   imports: [
     CommonModule,
     MainRoutingModule,
-    MatIconModule
+    MatIconModule,
+    ComponentsModule
   ]
 })
 export class MainModule { }
