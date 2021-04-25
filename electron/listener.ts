418ebcbf74df994cbc9f29d55966d6a87db36ea2
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-04-25 23:42:17 +0900

    create window manager

--- electron/listener.ts
+++ electron/listener.ts
@@ -1,8 +1,6 @@
 import { ipcMain } from "electron";
-import * as url from "url";
-import { BrowserWindow } from "electron"
-import path from "path"
 import { EVENTS } from "./events";
+import { windowManager } from "./singleton";
 
 
 ipcMain.on(EVENTS.DEBUG, () => {
@@ -10,55 +8,9 @@ ipcMain.on(EVENTS.DEBUG, () => {
 })
 
 ipcMain.on(EVENTS.OPEN_WINDOW.AUTH, () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    },
-    frame: false
-  })
-
-  const startUrl = (process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, 'k42un0k0passwordmanager/index.html'),
-    protocol: 'file:',
-    slashes: true
-  })) + "#/auth/login";
-  win.loadURL(startUrl);
+  windowManager.createWindow("#/auth/login");
 })
 
 ipcMain.on(EVENTS.OPEN_WINDOW.USER_ACCOUNT_MANAGER, () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    },
-    frame: false
-  })
-
-  const startUrl = (process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, 'k42un0k0passwordmanager/index.html'),
-    protocol: 'file:',
-    slashes: true
-  })) + "#/auth/login";
-  win.loadURL(startUrl);
-})
-
-ipcMain.on(EVENTS.OPEN_WINDOW.USER_ACCOUNT_MANAGER, () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js")
-    },
-    frame: false
-  })
-
-  const startUrl = (process.env.ELECTRON_START_URL || url.format({
-    pathname: path.join(__dirname, 'k42un0k0passwordmanager/index.html'),
-    protocol: 'file:',
-    slashes: true
-  })) + "#/user-account-manager";
-  win.loadURL(startUrl);
+  windowManager.createWindow("#/user-account-manager");
 })
