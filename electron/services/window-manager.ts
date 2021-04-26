6746a5b54b460e09c0fa2b03f4a585e150ba041e
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-04-26 20:34:12 +0900

    fix bug

--- electron/services/window-manager.ts
+++ electron/services/window-manager.ts
@@ -3,22 +3,24 @@ import { BrowserWindow } from "electron";
 import * as url from "url";
 
 export class WindowManager {
+  windowMap: Map<number, BrowserWindow> = new Map;
 
   createWindow(path: string) {
     const win = new BrowserWindow({
       width: 800,
       height: 600,
       webPreferences: {
-        preload: join(__dirname, "preload.js")
+        preload: join(__dirname, "../preload.js")
       },
       frame: false,
     })
 
     const startUrl = (process.env.ELECTRON_START_URL || url.format({
-      pathname: join(__dirname, 'k42un0k0passwordmanager/index.html'),
+      pathname: join(__dirname, '../k42un0k0passwordmanager/index.html'),
       protocol: 'file:',
       slashes: true
     })) + path;
     win.loadURL(startUrl);
+    this.windowMap.set(win.id, win);
   }
 }
