25265ca4952f562d0798358d840f391f8fe10ff3
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-05-14 18:45:05 +0900

    とりま完成

--- app/src/listener.ts
+++ app/src/listener.ts
@@ -1,12 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/init-declarations */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { CHANNELS } from 'lib';
 import type { IpcListenerMap } from './lib/ipc.service';
 import { WindowEnum } from './lib/window-manager';
-import { cipherService, iconService, keyService, windowManager } from './singleton';
+import { cipherService, iconService, ipcService, keyService, windowManager } from './singleton';
 
 export const windowManagerListener: IpcListenerMap = {
   [CHANNELS.windowService.close]: (e) => {
@@ -17,39 +12,8 @@ export const windowManagerListener: IpcListenerMap = {
   [CHANNELS.windowService.userAccountManager]: async () => windowManager.createWindow(WindowEnum.userAccountManager),
 };
 
-function createListhener<T extends Record<string, string>>(
-  channel: T,
-  service: Record<keyof T, (...args: any[]) => any>,
-  options?: {
-    takeEventFunc?: (keyof T)[];
-    syncFunc?: (keyof T)[];
-  }
-): IpcListenerMap {
-  return Object.entries<string>(channel).reduce<IpcListenerMap>((pre, [key, value]) => {
-    const isTakeEvent = options?.takeEventFunc?.find((v) => v === key) != null;
-    const isSync = options?.syncFunc?.find((v) => v === key) != null;
-    const listener = (e: any, ...args: any[]): any => {
-      let returnValue: any;
-      if (isTakeEvent) {
-        returnValue = service[key as keyof T](e, ...args);
-      } else {
-        returnValue = service[key as keyof T](...args);
-      }
-
-      if (isSync) {
-        e.returnValue = returnValue;
-        return;
-      }
-      return returnValue;
-    };
-    listener.isSync = isSync;
-    pre[value] = listener;
-    return pre;
-  }, {});
-}
-
-export const iconSerciseListener = createListhener(CHANNELS.iconService, iconService);
-export const keySerciseListener = createListhener(CHANNELS.keyService, keyService);
-export const cipherSerciseListener = createListhener(CHANNELS.cipherService, cipherService, {
+export const iconSerciseListener = ipcService.createListhenerMap(CHANNELS.iconService, iconService);
+export const keySerciseListener = ipcService.createListhenerMap(CHANNELS.keyService, keyService);
+export const cipherSerciseListener = ipcService.createListhenerMap(CHANNELS.cipherService, cipherService, {
   syncFunc: ['cipher', 'decipher'],
 });
